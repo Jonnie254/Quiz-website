2c4318df9d55a75916bde1146116e50a1756e0be
jonnie254 <johnkamanu254@proton.me>
2024-01-26 14:55:08 +0300

    added some codes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+//assigning the variables
 const startBtn = document.querySelector('.start-btn');
 const popupInfo = document.querySelector('.popup-info');
 const exitBtn = document.querySelector('.exit-btn');
 const main = document.querySelector('.main');
+const continueBtn = document.querySelector(".continue-btn");
+const quizSection = document.querySelector('.quiz-section');
 
 // Add a click event listener to the startBtn element
 startBtn.onclick = () => {
@@ -15,3 +18,11 @@ exitBtn.onclick = () => {
     popupInfo.classList.remove('active');
     main.classList.remove('active');
 }
+
+//Add a click event listener to the continuebtn element
+continueBtn.onclick = () => {
+    quizSection.classList.add('active');
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
+}
+
