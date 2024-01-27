047a381cf378c17cebe09ff12f0b6258ad102eb9
jonnie254 <johnkamanu254@proton.me>
2024-01-27 16:53:42 +0300

    added some codes

---
+++ questions.js
@@ -0,0 +1,59 @@
+/*define an array to represent each questions*/
+let questions =[
+    {
+        numb : 1,
+        question:"What does HTML stand for ?",
+        answer:"C. Hyper Text Markup Language",
+        options:[
+             "A. Hyper Type Multi Language",
+             "B. Hyper Text Multiple Language",                  
+             "C. Hyper Text Markup Language",                    
+             "D. Home Text Multi Language"
+        ]
+    },
+    {
+        numb : 2,
+        question:"What does CSS stand for?",
+        answer:"A. Cascading Style Sheet",
+        options:[
+            "A. Cascading Style Sheet",
+            "B. Cute Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Cohedal Style Sheet"
+        ]
+    },
+    {
+        numb : 3,
+        question:"What does PHP stand for?",
+        answer:"D. Programming Hypertext Processor",
+        options:[
+            "A. Hypertext Processor",
+            "B. Hometext Programming",
+            "C. Hypertext Programming",
+            "D. Programming Hypertext Processor"
+        ]
+    },
+    {
+        numb : 4,
+        question:"What does sql stand for?",
+        answer:"D. Structured Query Language",
+        options:[
+            "A. Strength Query Language",
+            "B. Style Query Language",
+            "C. Science Query Language",
+            "D  Structured Query Language"
+            
+        ]
+    },
+    {
+        numb : 5,
+        question:"What does XML stand for?",
+        answer:"D. Extensible Markup Language",
+        options:[
+            "A. Excellent Multiple Language",
+            "B. Explore Multiple Languages",
+            "C. Extra Markup Language",
+            "D. Extensible Markup Language"
+        ]
+    },
+];
